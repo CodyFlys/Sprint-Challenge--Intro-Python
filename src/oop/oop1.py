f4494e71222febee78a03d7db5a18cd82ffc17e0
Cody <codysolomon15@gmail.com>
2020-06-05 09:07:41 -0700

    completed sprint, mvp no stretch

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,32 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle():
+    '''base class'''
+    pass
+
+''' START OF HIERARCHY FOR GROUND VEHICLES '''
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+''' END OF HIERARCHY FOR GROUND VEHICLES '''
+''' START OF HIERARCHY FOR AIR VEHICLES '''
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+''' END OF HIERARCHY FOR AIR VEHICLES '''
